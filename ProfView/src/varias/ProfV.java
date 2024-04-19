4bb6e5f8e08ff2a6a775cf29caeb4c1de2983626
Student <Student@DESKTOP-M6VKDDK>
2024-04-19 08:19:57 +0800

    Professor View Window

---
+++ ProfView/src/varias/ProfV.java
@@ -0,0 +1,95 @@
+package varias;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JTextField;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JButton;
+
+public class ProfV {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ProfV window = new ProfV();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ProfV() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setEnabled(false);
+		frame.getContentPane().setBackground(Color.PINK);
+		frame.setBounds(100, 100, 991, 610);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(0, -11, 977, 98);
+		lblNewLabel.setIcon(new ImageIcon("d:\\Users\\Student\\Desktop\\Screenshot 2024-04-19 081803.png"));
+		frame.getContentPane().add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("Grades");
+		btnNewButton.setBounds(188, 98, 112, 34);
+		btnNewButton.setForeground(new Color(255, 255, 255));
+		btnNewButton.setBackground(new Color(204, 4, 34));
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Profile");
+		btnNewButton_1.setBounds(33, 98, 112, 34);
+		btnNewButton_1.setForeground(new Color(255, 255, 255));
+		btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnNewButton_1.setBackground(new Color(204, 4, 34));
+		frame.getContentPane().add(btnNewButton_1);
+		
+		JButton btnSchedule = new JButton("Schedule");
+		btnSchedule.setBounds(342, 98, 112, 34);
+		btnSchedule.setForeground(new Color(255, 255, 255));
+		btnSchedule.setBackground(new Color(204, 4, 34));
+		btnSchedule.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		frame.getContentPane().add(btnSchedule);
+		
+		JButton btnLogout = new JButton("Log-out");
+		btnLogout.setBounds(832, 98, 112, 34);
+		btnLogout.setForeground(new Color(255, 255, 255));
+		btnLogout.setBackground(new Color(204, 4, 34));
+		btnLogout.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		frame.getContentPane().add(btnLogout);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(-10, 78, 987, 68);
+		panel.setBackground(new Color(149, 0, 0));
+		frame.getContentPane().add(panel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Welcome, ");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel_1.setBounds(44, 163, 74, 24);
+		frame.getContentPane().add(lblNewLabel_1);
+	}
+}
