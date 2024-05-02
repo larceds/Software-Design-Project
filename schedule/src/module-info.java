40c943afc51846cab3a0d2f3fbdab7f4ed993043
PayeCent <fayemillicent.sanata.cvt@eac.edu.ph>
2024-05-03 07:30:33 +0800

    design

---
+++ schedule/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module schedule {
+	requires java.desktop;
+}
---
+++ schedule/src/schedule/Schedule.java
@@ -0,0 +1,204 @@
+package schedule;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JTextField;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.JList;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JComboBox;
+import javax.swing.JScrollPane;
+import javax.swing.DefaultComboBoxModel;
+
+public class Schedule {
+
+	private JFrame frame;
+	private JTable table;
+	private JTable table_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Schedule window = new Schedule();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Schedule() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setEnabled(false);
+		frame.getContentPane().setBackground(Color.PINK);
+		frame.setBounds(100, 100, 991, 610);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnNewButton = new JButton("Grades");
+		btnNewButton.setBounds(132, 98, 74, 24);
+		btnNewButton.setForeground(new Color(0, 0, 0));
+		btnNewButton.setBackground(new Color(204, 4, 34));
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Profile");
+		btnNewButton_1.setBounds(33, 98, 74, 24);
+		btnNewButton_1.setForeground(new Color(0, 0, 0));
+		btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnNewButton_1.setBackground(new Color(204, 4, 34));
+		frame.getContentPane().add(btnNewButton_1);
+		
+		JButton btnSchedule = new JButton("Schedule");
+		btnSchedule.setBounds(232, 98, 77, 24);
+		btnSchedule.setForeground(new Color(0, 0, 0));
+		btnSchedule.setBackground(new Color(204, 4, 34));
+		btnSchedule.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		frame.getContentPane().add(btnSchedule);
+		
+		JButton btnLogout = new JButton("Log-out");
+		btnLogout.setBounds(830, 98, 114, 24);
+		btnLogout.setForeground(Color.BLACK);
+		btnLogout.setBackground(new Color(204, 4, 34));
+		btnLogout.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		frame.getContentPane().add(btnLogout);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(-10, 78, 987, 59);
+		panel.setBackground(new Color(149, 0, 0));
+		frame.getContentPane().add(panel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Welcome, ");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblNewLabel_1.setBounds(33, 147, 74, 24);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		JList list = new JList();
+		list.setBackground(Color.BLACK);
+		list.setBounds(165, 339, 156, -89);
+		frame.getContentPane().add(list);
+		
+		JLabel lblNewLabel_2 = new JLabel("Sem:");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblNewLabel_2.setBounds(210, 181, 45, 13);
+		frame.getContentPane().add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("School Year:");
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblNewLabel_3.setBounds(33, 181, 88, 13);
+		frame.getContentPane().add(lblNewLabel_3);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(33, 209, 911, 202);
+		frame.getContentPane().add(scrollPane);
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
+		table.setFont(new Font("Tahoma", Font.BOLD, 13));
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+			},
+			new String[] {
+				"#", "Subject Code", "Description", "Lec", "Lab", "Units", "Schedule"
+			}
+		) {
+			Class[] columnTypes = new Class[] {
+				Object.class, Double.class, Object.class, Object.class, Object.class, Object.class, Object.class
+			};
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+		});
+		table.getColumnModel().getColumn(0).setPreferredWidth(24);
+		table.getColumnModel().getColumn(2).setPreferredWidth(341);
+		table.getColumnModel().getColumn(3).setPreferredWidth(34);
+		table.getColumnModel().getColumn(4).setPreferredWidth(34);
+		table.getColumnModel().getColumn(5).setPreferredWidth(40);
+		table.getColumnModel().getColumn(6).setPreferredWidth(297);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setFont(new Font("Tahoma", Font.BOLD, 10));
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"2022 - 2023", "2023 - 2024", "2024 - 2025"}));
+		comboBox.setBounds(112, 178, 88, 21);
+		frame.getContentPane().add(comboBox);
+		
+		JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"First", "Second", "Summer"}));
+		comboBox_1.setFont(new Font("Tahoma", Font.BOLD, 11));
+		comboBox_1.setBounds(246, 178, 88, 21);
+		frame.getContentPane().add(comboBox_1);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\ACER\\Downloads\\437055423_1161141725028274_4161512821301030128_n.jpg"));
+		lblNewLabel.setBounds(0, 0, 977, 82);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		scrollPane_1.setBounds(15, 421, 952, 142);
+		frame.getContentPane().add(scrollPane_1);
+		
+		table_1 = new JTable();
+		scrollPane_1.setViewportView(table_1);
+		table_1.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null},
+			},
+			new String[] {
+				"", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
+			}
+		));
+		
+		JLabel lblNewLabel_4 = new JLabel("New label");
+		lblNewLabel_4.setBackground(new Color(240, 240, 240));
+		lblNewLabel_4.setIcon(new ImageIcon("C:\\Users\\ACER\\Downloads\\434637222_2369367910121302_5462547168545990256_n.jpg"));
+		lblNewLabel_4.setBounds(0, 136, 977, 437);
+		frame.getContentPane().add(lblNewLabel_4);
+	}
+}
