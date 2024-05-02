d5e1d5f61febc543e3839715b54dba4986c5f54d
larceds <carlenrish.santos.cvt@eac.edu.ph>
2024-05-03 07:49:03 +0800

    crs

--- SDFinalProg/src/admWin/crs.java
+++ SDFinalProg/src/admWin/crs.java
@@ -7,6 +7,7 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JToolBar;
+import javax.swing.JTabbedPane;
 
 public class crs {
 
@@ -44,20 +45,36 @@ public class crs {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JButton btnNewButton = new JButton("Programs");
-		btnNewButton.setBounds(56, 104, 89, 23);
-		frame.getContentPane().add(btnNewButton);
-		
-		JButton btnNewButton_1 = new JButton("Slot Monitoring");
-		btnNewButton_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnNewButton_1.setBounds(56, 149, 103, 23);
-		frame.getContentPane().add(btnNewButton_1);
-		
-		JButton btnNewButton_2 = new JButton("");
-		btnNewButton_2.setBounds(56, 181, 89, 23);
+		JButton btnNewButton_2 = new JButton("Profile");
+		btnNewButton_2.setBounds(35, 70, 89, 23);
 		frame.getContentPane().add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("Grade");
+		btnNewButton_3.setBounds(120, 70, 89, 23);
+		frame.getContentPane().add(btnNewButton_3);
+		
+		JButton btnNewButton_4 = new JButton("Schedule");
+		btnNewButton_4.setBounds(205, 70, 89, 23);
+		frame.getContentPane().add(btnNewButton_4);
+		
+		JButton btnNewButton_5 = new JButton("Courses");
+		btnNewButton_5.setBounds(291, 70, 89, 23);
+		frame.getContentPane().add(btnNewButton_5);
+		
+		JButton btnNewButton_6 = new JButton("Account");
+		btnNewButton_6.setBounds(378, 70, 89, 23);
+		frame.getContentPane().add(btnNewButton_6);
+		
+		JButton button = new JButton("Student");
+		button.setBounds(465, 70, 89, 23);
+		frame.getContentPane().add(button);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(170, 166, 762, 349);
+		frame.getContentPane().add(tabbedPane);
+		
+		JButton btnNewButton = new JButton("Logout");
+		btnNewButton.setBounds(832, 70, 89, 23);
+		frame.getContentPane().add(btnNewButton);
 	}
 }
